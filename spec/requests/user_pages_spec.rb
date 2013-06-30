2cfead31778c21351b7ac7cbf37513a035d852eb
morron <dk@totouch.ru>
2013-07-01 02:05:12 +0400

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "UserPages" do
+
+  subject { page }
+
+  describe "singup page" do
+  	before { visit  singup_path }
+
+  	it { should have_selector('h1', text: 'Sing up') }
+  	it { should have_selector('title', text: full_title('')) }
+  end
+end
